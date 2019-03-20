5e394b86346586773d5f72f2e429fd6da4fead01
Aditya Eka <aditya.eka409@gmail.com>
2019-03-20 20:34:29 +0700

    update

--- assets/js/dps.js
+++ assets/js/dps.js
@@ -1,91 +1,99 @@
+
 var pesawat;
-function setup() {
+function setup(){
 	createCanvas(1000, 500);
 	frameRate(60);
 	
 	pesawat = new Pesawat();
+	// alert('Press enter to eject bomb');
+	textSize(20);
 }
 
-function draw() {
+function draw(){
 	background(0);
 	pesawat.draw();
-	fill('#66330e');
+	fill('#8B4513');
 	rect(0, height-25, width, height);
 }
 
-function keyPressed() {
-	if (keyCode == ENTER) {
+var flag = true;
+function keyPressed(){
+	if(keyCode == ENTER){
 		pesawat.eject();
+	} else if(keyCode == 32){
+		frameRate(flag ? 0 : 60);
+		flag = !flag;
 	}
 }
 
-function Pesawat() {
+function Pesawat(){
 	var pos = [0, 20];
-	var speed = 2;
+	var speed = 100 / 60;
 	var bombs = [];
 	var img = loadImage('assets/img/plane.png');
-
-	this.draw = function() {
-		// fill(255);
-		// rect(pos[0], pos[1], 100, 25);
+	this.draw = function(){
+		//fill(255);
+		//rect(pos[0], pos[1], 100, 25);
 		image(img, pos[0], pos[1]);
-		pos[0] += speed;
+		pos[0]+=speed;
 		
-		if (pos[0] >= width) {
+		if(pos[0] >= width){
 			pos[0] = 0;
 			bombs = [];
 		}
 		
-		for (var i = 0; i < bombs.length; i++) {
-			if (bombs[i] != null) {
+		for(var i = 0; i<bombs.length; i++){
+			if(bombs[i] != null){
 				bombs[i].draw();
 			}
-			if (bombs[i].getPos()[1] >= height) {
+			if(bombs[i].getPos()[1] >= height){
 				bombs.splice(i, 1);
 			}
 		}
 	}
-
-	this.eject = function() {
+	this.eject = function(){
 		bombs.push(new Bomb(pos[0]+48, pos[1]+48, speed));
 	}
 }
 
-function Bomb(x, y, v0) {
+function Bomb(x, y, v0){
+	v0 = v0 * 60;
 	var pos = [x, y];
 	var t = 0;
 	var pi = 3.1415926535897;
 	var sudut = 0;
 	var theta = sudut/(2*pi);
-	var g = 0.03;
+	var g = 300;
 	var img = loadImage('assets/img/bomb.png');
 	var explode = loadImage('assets/img/explode.png');
 	var sound = loadSound('assets/sound/explode.mp3');
-
-	this.getPos = function() {
+	this.getPos = function(){
 		return pos;
 	}
 
-	this.draw = function() {
-		if (pos[1] >= height-50) {
+	this.draw = function(){
+		if(pos[1] >= height-50){
 			image(explode, pos[0]-50, height-100);
-			if (!sound.isPlaying()) {
+			if(!sound.isPlaying()){
 				sound.play();
 			}
 		} else {
 			image(img, pos[0]-25, pos[1]);
 		}
+		fill(255);
+		var tt = t/60;
+		//text('('+v0+','+pos[1]+')', pos[0], pos[1]);
+		console.log(theta);
+		pos[0] = x + v0 * cos(theta) * tt;
+		pos[1] = y + -1 * (v0 * sin(theta) * tt - 0.5 * g * tt *tt);
+		//v0 = v0*sin(theta)
+		vx = v0 * cos(theta);
+		vy = v0 * sin(theta) - g * tt;
+		text('Vx: '+vx.toFixed(2)+'m/s', pos[0]+15, pos[1]+20);
+		text('Vy: '+(-vy).toFixed(2)+'m/s', pos[0]+15, pos[1]+40);
 		
-		pos[0] = x + v0 * cos(theta) * t;
-		pos[1] = y + - 1 * (v0 * sin(theta) * t - 0.5 * g * t * t);
+		text('t: '+ tt.toFixed(2)+'s', pos[0]+15, pos[1]+60);
 		t++;
 	}
-
-	this.getKecepatanX = function(v0, theta) {
-		return v0 * cos(theta);
-	}
-
-	this.getKecepatanY = function(v0, theta, g, t) {
-		return v0 * sin(theta) - g * t;
-	}
 }
+
